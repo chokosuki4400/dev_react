9fea2bbea14919ea644ce1bc0f66bfdde470769f
EJWORKS\ohnishi-y <y.ohnishi@ejworks.com>
2017-02-16 15:05:22 +0900

    一旦コミット

--- babel/src/app.jsx
+++ babel/src/app.jsx
@@ -1,4 +1,65 @@
+const classHogeName="foo";
+const props={
+  id:"bar",
+  className:classHogeName
+};
+const hoge={
+  id:"bar",
+  className:classHogeName
+};
+
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <h1 {...props}>Hello, world!</h1>
+    <p {...hoge}>HOGE</p>
+  </div>,
   document.getElementById('app')
 );
+
+
+//コンポーネントの作成
+var MyComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>texttext</h2>
+      </div>
+    )
+  }
+});
+
+//コンポーネントの描画
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('app02')
+);
+
+//コンポーネントの作成
+function MyH1() {
+  return (
+    <h1>Hello, world!</h1>
+  )
+}
+function MyH2() {
+  return (
+    <h2>texttext</h2>
+  )
+}
+function MyComponent() {
+  return (
+    <div>
+      <MyH1 />
+      <MyH2 />
+    </div>
+  )
+};
+
+//コンポーネントの描画
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('app03')
+);
+
+
